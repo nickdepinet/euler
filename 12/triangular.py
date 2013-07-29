edab647f34b5793caf361ebb97e241a1d37320d1
Nick Depinet <depinetnick@gmail.com>
2013-07-28 23:09:05 -0400

    Working solution for #12, way too slow

---
+++ 12/triangular.py
@@ -0,0 +1,13 @@
+from itertools import count
+"""
+	Project Euler #12: what is the first triangle number of have over 500 divisors?
+	Author: Nick Depinet
+"""
+
+def numFactors(x):
+	return len(filter(lambda i: x % i == 0, xrange(1, x+1)))
+def triangles():
+	for x in count(1):
+		yield sum(y for y in (xrange(0,x+1)))
+
+print next(x for x in triangles() if numFactors(x) > 500)
