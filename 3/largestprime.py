6d07e1939983d73b0bb5a40f0cdef19d05be2f07
Nick Depinet <depinetnick@gmail.com>
2013-07-29 02:41:03 -0400

    removed unecessary use of collections

--- 3/largestprime.py
+++ 3/largestprime.py
@@ -1,5 +1,4 @@
 from itertools import chain, imap, ifilter
-import collections
 from time import time
 """
 	Project Euler #3: Largest Prime Factor of 600851475143
@@ -16,6 +15,6 @@ def timeit(func):
 
 @timeit
 def solve():	
-	return collections.deque(sorted(f for f in chain.from_iterable(imap(lambda r: (r, 600851475143 / r), ifilter(lambda x: 600851475143 % x == 0, xrange(1, int(600851475143**0.5+1))))) if not any(ifilter(lambda y: f % y == 0, xrange(2, int(f**0.5+1))))), maxlen=1)
+	return sorted(f for f in chain.from_iterable(imap(lambda r: (r, 600851475143 / r), ifilter(lambda x: 600851475143 % x == 0, xrange(1, int(600851475143**0.5+1))))) if not any(ifilter(lambda y: f % y == 0, xrange(2, int(f**0.5+1)))))[-1]
 
 print solve()
