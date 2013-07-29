3b5a1cc4f71d8f368fa56b25980f21379e795ddf
Nick Depinet <depinetnick@gmail.com>
2013-07-29 02:27:54 -0400

    fixing up and adding timing

--- 3/largestprime.py
+++ 3/largestprime.py
@@ -1,8 +1,21 @@
 from itertools import chain, imap, ifilter
 import collections
+from time import time
 """
 	Project Euler #3: Largest Prime Factor of 600851475143
 	Author: Nick Depinet
 """
+def timeit(func):
+	def timeitandrun(*args, **kwargs):
+		start = time()
+		results = func(*args, **kwargs)
+		end = time()
+		print ''.join(['took: ', str(end-start)])
+		return results
+	return timeitandrun
 
-print collections.deque(sorted(f for f in chain.from_iterable(imap(lambda r: (r, 600851475143 / r), ifilter(lambda x: 600851475143 % x == 0, xrange(1, 600851475143**0.5+1)))) if not any(ifilter(lambda y: f % y == 0, xrange(2, f**0.5+1)))))
+@timeit
+def solve():	
+	return collections.deque(sorted(f for f in chain.from_iterable(imap(lambda r: (r, 600851475143 / r), ifilter(lambda x: 600851475143 % x == 0, xrange(1, 600851475143**0.5+1)))) if not any(ifilter(lambda y: f % y == 0, xrange(2, f**0.5+1)))), maxlen=1)
+
+print solve()
